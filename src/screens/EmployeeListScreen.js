08def43f19d297d45b52068c7d7ea26379fd52b0
Mohd Nahid <mnahid.amkh@gmail.com>
2022-12-28 08:21:30 +0530

    kiosk app chages

--- src/screens/EmployeeListScreen.js
+++ src/screens/EmployeeListScreen.js
@@ -1,97 +1,98 @@
 import React from 'react'
 import { View, Text, TextInput, ScrollView, Image } from 'react-native'
-import { NativeBaseProvider} from 'native-base'
+import { NativeBaseProvider } from 'native-base'
 
+import dsmTypographyStyle from '../assets/stylesheets/dsmStyles/dsmTypographyStyle';
 import externalStyles from '../assets/stylesheets/externalStyle';
 
 const EmployeeListScreen = () => {
-    return (
-        <NativeBaseProvider>
-          <View style={externalStyles.setttingsEmplistContainer}>
-            <View style={externalStyles.settingsEmpListSearchBox}>
-              <TextInput
-                placeholder="Enter Email / Mobile Number"
-                style={externalStyles.Settingsinput}
-                autoCapitalize="none"
-                autoCorrect={false}
-              />
-            </View>
-            <View style={externalStyles.settingsEmpListHeader}>
-              <Text style={externalStyles.settingsEmpNameHeading}>Employee Name</Text>
-              <Text style={externalStyles.settingsEmpLastUpdatedText}>Updated On</Text>
-            </View>
-            <ScrollView>
-              <View style={externalStyles.settingsEmpData}>
-                <View style={externalStyles.settingsEmpDetails}>
-                  <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
-                    <Text style={externalStyles.settingsEmpNameFirstText}>J</Text>
-                    <View style={{ marginLeft: 12 }}>
-                      <Text style={externalStyles.settingsEmpName}>John Doe</Text>
-                      <Text style={externalStyles.settingsEmpId}>ID: 1212121</Text>
-                    </View>
-                  </View>
-                  <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
-                    <Image source={require('../assets/icons/clock.png')} style={{ marginRight: 4 }} />
-                    <Text style={externalStyles.settingsEmpUpdatedDay}>10:00am, 22 Sep</Text>
-                  </View>
-                </View>
-                <View style={externalStyles.settingsEmpDetails}>
-                  <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
-                    <Text style={externalStyles.settingsEmpNameFirstText}>J</Text>
-                    <View style={{ marginLeft: 12 }}>
-                      <Text style={externalStyles.settingsEmpName}>John Doe</Text>
-                      <Text style={externalStyles.settingsEmpId}>ID: 1212121</Text>
-                    </View>
-                  </View>
-                  <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
-                    <Image source={require('../assets/icons/clock.png')} style={{ marginRight: 4 }} />
-                    <Text style={externalStyles.settingsEmpUpdatedDay}>10:00am, 22 Sep</Text>
-                  </View>
-                </View>
-                <View style={externalStyles.settingsEmpDetails}>
-                  <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
-                    <Text style={externalStyles.settingsEmpNameFirstText}>M</Text>
-                    <View style={{ marginLeft: 12 }}>
-                      <Text style={externalStyles.settingsEmpName}>Manoj Dey</Text>
-                      <Text style={externalStyles.settingsEmpId}>ID: 1212121</Text>
-                    </View>
-                  </View>
-                  <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
-                    <Image source={require('../assets/icons/clock.png')} style={{ marginRight: 4 }} />
-                    <Text style={externalStyles.settingsEmpUpdatedDay}>10:00am, 22 Sep</Text>
-                  </View>
-                </View>
-                <View style={externalStyles.settingsEmpDetails}>
-                  <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
-                    <Text style={externalStyles.settingsEmpNameFirstText}>J</Text>
-                    <View style={{ marginLeft: 12 }}>
-                      <Text style={externalStyles.settingsEmpName}>John Doe</Text>
-                      <Text style={externalStyles.settingsEmpId}>ID: 1212121</Text>
-                    </View>
-                  </View>
-                  <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
-                    <Image source={require('../assets/icons/clock.png')} style={{ marginRight: 4 }} />
-                    <Text style={externalStyles.settingsEmpUpdatedDay}>10:00am, 22 Sep</Text>
-                  </View>
-                </View>
-                <View style={externalStyles.settingsEmpDetails}>
-                  <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
-                    <Text style={externalStyles.settingsEmpNameFirstText}>R</Text>
-                    <View style={{ marginLeft: 12 }}>
-                      <Text style={externalStyles.settingsEmpName}>Ravi Kumar</Text>
-                      <Text style={externalStyles.settingsEmpId}>ID: 1212121</Text>
-                    </View>
-                  </View>
-                  <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
-                    <Image source={require('../assets/icons/clock.png')} style={{ marginRight: 4 }} />
-                    <Text style={externalStyles.settingsEmpUpdatedDay}>10:00am, 22 Sep</Text>
-                  </View>
+  return (
+    <NativeBaseProvider>
+      <View style={externalStyles.setttingsEmplistContainer}>
+        <View style={externalStyles.settingsEmpListSearchBox}>
+          <TextInput
+            placeholder="Enter Email / Mobile Number"
+            style={externalStyles.Settingsinput}
+            autoCapitalize="none"
+            autoCorrect={false}
+          />
+        </View>
+        <View style={externalStyles.settingsEmpListHeader}>
+          <Text style={dsmTypographyStyle.mDsmMediumSemiBold}>Employee Name</Text>
+          <Text style={externalStyles.settingsEmpLastUpdatedText}>Updated On</Text>
+        </View>
+        <ScrollView>
+          <View style={externalStyles.settingsEmpData}>
+            <View style={externalStyles.settingsEmpDetails}>
+              <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+                <Text style={externalStyles.settingsEmpNameFirstText}>J</Text>
+                <View style={{ marginLeft: 12 }}>
+                  <Text style={dsmTypographyStyle.mDsmMediumNormal}>John Doe</Text>
+                  <Text style={externalStyles.settingsEmpId}>ID: 1212121</Text>
                 </View>
               </View>
-            </ScrollView>
+              <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+                <Image source={require('../assets/icons/clock.png')} style={{ marginRight: 4 }} />
+                <Text style={dsmTypographyStyle.mDsmTinyNormal}>10:00am, 22 Sep</Text>
+              </View>
+            </View>
+            <View style={externalStyles.settingsEmpDetails}>
+              <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+                <Text style={externalStyles.settingsEmpNameFirstText}>J</Text>
+                <View style={{ marginLeft: 12 }}>
+                  <Text style={dsmTypographyStyle.mDsmMediumNormal}>John Doe</Text>
+                  <Text style={externalStyles.settingsEmpId}>ID: 1212121</Text>
+                </View>
+              </View>
+              <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+                <Image source={require('../assets/icons/clock.png')} style={{ marginRight: 4 }} />
+                <Text style={dsmTypographyStyle.mDsmTinyNormal}>10:00am, 22 Sep</Text>
+              </View>
+            </View>
+            <View style={externalStyles.settingsEmpDetails}>
+              <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+                <Text style={externalStyles.settingsEmpNameFirstText}>M</Text>
+                <View style={{ marginLeft: 12 }}>
+                  <Text style={dsmTypographyStyle.mDsmMediumNormal}>John Doe</Text>
+                  <Text style={externalStyles.settingsEmpId}>ID: 1212121</Text>
+                </View>
+              </View>
+              <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+                <Image source={require('../assets/icons/clock.png')} style={{ marginRight: 4 }} />
+                <Text style={dsmTypographyStyle.mDsmTinyNormal}>10:00am, 22 Sep</Text>
+              </View>
+            </View>
+            <View style={externalStyles.settingsEmpDetails}>
+              <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+                <Text style={externalStyles.settingsEmpNameFirstText}>J</Text>
+                <View style={{ marginLeft: 12 }}>
+                  <Text style={dsmTypographyStyle.mDsmMediumNormal}>John Doe</Text>
+                  <Text style={externalStyles.settingsEmpId}>ID: 1212121</Text>
+                </View>
+              </View>
+              <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+                <Image source={require('../assets/icons/clock.png')} style={{ marginRight: 4 }} />
+                <Text style={dsmTypographyStyle.mDsmTinyNormal}>10:00am, 22 Sep</Text>
+              </View>
+            </View>
+            <View style={externalStyles.settingsEmpDetails}>
+              <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+                <Text style={externalStyles.settingsEmpNameFirstText}>R</Text>
+                <View style={{ marginLeft: 12 }}>
+                  <Text style={dsmTypographyStyle.mDsmMediumNormal}>John Doe</Text>
+                  <Text style={externalStyles.settingsEmpId}>ID: 1212121</Text>
+                </View>
+              </View>
+              <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+                <Image source={require('../assets/icons/clock.png')} style={{ marginRight: 4 }} />
+                <Text style={dsmTypographyStyle.mDsmTinyNormal}>10:00am, 22 Sep</Text>
+              </View>
+            </View>
           </View>
-        </NativeBaseProvider>
-      );
+        </ScrollView>
+      </View>
+    </NativeBaseProvider>
+  );
 }
 
 export default EmployeeListScreen
